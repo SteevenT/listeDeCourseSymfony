44994f180a4713f86d05ba6bc9e07ad88ce20733
SteevenT <tillier.steeven36@gmail.com>
2021-09-22 16:45:57 +0200

    fin TP

---
+++ src/Controller/CourseController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Item;
+use App\Form\ItemType;
+use App\Repository\ItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CourseController extends AbstractController
+{
+    /**
+     * @Route("/", name="course")
+     */
+    public function index(Request $request, ItemRepository $repo): Response
+    {
+        $items = $repo->findAll();
+
+        $item = new Item();
+        
+        $formItem = $this->createForm(ItemType::class,$item);
+        $formItem->handleRequest($request);
+
+        // si le form est validé.
+        if ($formItem->isSubmitted()){
+            $item->setDateAdd(new \DateTime('now'));
+            $item->setIsBuy(false);
+            if($item->getQuantite() == null || $item->getQuantite() == 0)
+            {
+                $item->setQuantite(1);
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+
+            return $this->redirectToRoute('course');
+        }
+
+        return $this->render('course/index.html.twig',[
+            'formItem' => $formItem->createView(),
+            'items' => $items,
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     */
+    public function edit(Item $item, EntityManagerInterface $em): Response
+    {
+        $item->setIsBuy(true);
+
+        $em->persist($item);
+        $em->flush();
+
+        return $this->redirectToRoute('course');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(Item $item, EntityManagerInterface $em): Response
+    {
+        // $em = $this->getDoctrine()->getManager();
+        $em->remove($item);
+        $em->flush();
+        return $this->redirectToRoute('course');
+    }
+}
---
+++ src/Entity/Item.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ItemRepository::class)
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateAdd;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBuy;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $quantite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateAdd(): ?\DateTimeInterface
+    {
+        return $this->dateAdd;
+    }
+
+    public function setDateAdd(\DateTimeInterface $dateAdd): self
+    {
+        $this->dateAdd = $dateAdd;
+
+        return $this;
+    }
+
+    public function getIsBuy(): ?bool
+    {
+        return $this->isBuy;
+    }
+
+    public function setIsBuy(bool $isBuy): self
+    {
+        $this->isBuy = $isBuy;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(?int $quantite): self
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+}
